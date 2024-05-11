5a7e44de7d56e70ed7bcd9886b5f40679d3c38b6
Henning Thomann <102948323+default-myfault@users.noreply.github.com>
2024-05-11 11:35:40 +0200

    Create .wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,24 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.color_scheme = "purplepeter"
+
+config.enable_tab_bar = false
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.font = wezterm.font({
+	family = "Fira Code",
+	harfbuzz_features = { "calt=1", "clig=1", "liga=1" },
+})
+config.font_size = 13
+config.force_reverse_video_cursor = true
+
+config.default_domain = "WSL:Ubuntu"
+
+return config
